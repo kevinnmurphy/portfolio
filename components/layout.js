18e9b68003f007fdd6b4520f0edf4b65935f4d28
KevinnMurphy <kevinnealmurphy@gmail.com>
2021-01-04 13:40:31 -0500

    add locale, remove font

--- components/layout.js
+++ components/layout.js
@@ -12,16 +12,17 @@ export default function Layout({ children, home }) {
     <div className={styles.container}>
       <Head>
         <link rel='icon' href='/favicon.ico' />
-        <link
+        {/* <link
           href='https://fonts.googleapis.com/css2?family=Catamaran:wght@900&display=swap'
           rel='stylesheet'
-        ></link>
+        ></link> */}
         <meta
           property='og:image'
           content={`https://og-image.now.sh/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
+        <meta property='og:locale' content='en' />
       </Head>
       <Navbar />
       <header className={styles.header}>
