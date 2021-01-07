743cd7282a46e04da56950810fb8f2be3c6632b8
KevinnMurphy <kevinnealmurphy@gmail.com>
2021-01-06 19:34:39 -0500

    runtime caching

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    runtimeCaching,
   },
   i18n: {
     locales: ['en-US'],
